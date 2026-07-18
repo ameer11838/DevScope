c0aab62fe3aaac697da7a6d6d46845510ad3eec1
Ameer Hassan <ameer.hassan726@gmail.com>
2026-07-18 16:53:57 +0000

    Updated GET Request

--- devscope.py
+++ devscope.py
@@ -1,16 +1,14 @@
 import requests
 
-# GitHub username to analyze
 username = "ameer11838"
 
-# GitHub API URL
 url = f"https://api.github.com/users/{username}"
 
-# Send GET request
 response = requests.get(url)
 
-# Convert JSON response into a dictionary
 data = response.json()
 
-# Print the whole JSON response
-print(data)
+print("Name:", data["name"])
+print("Username:", data["login"])
+print("Public Repositories:", data["public_repos"])
+print("Followers:", data["followers"])
